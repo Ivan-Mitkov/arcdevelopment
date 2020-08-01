d410683219a888ef9512a16086b5961be0e4353c
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-08-01 11:56:28 +0300

    add next.js initial config

---
+++ nextjs/src/ui/CallToAction.js
@@ -0,0 +1,119 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import ButtonArrow from "../../components/ui/ButtonArrow";
+import Typography from "@material-ui/core/Typography";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { makeStyles, useTheme } from "@material-ui/styles";
+import { Link } from "react-router-dom";
+
+// import background from "../../assets/background.jpg";
+// import mobileBackground from "../../assets/mobileBackground.jpg";
+
+const useStyle = makeStyles((theme) => ({
+  btnContainer: {
+    marginTop: "1rem",
+  },
+  learnBtnHero: {
+    ...theme.typography.learnBtn,
+  },
+  background: {
+    backgroundImage: url("/assets/background.jpg"),
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%",
+    height: "30rem",
+    //fixed image
+    backgroundAttachment: "fixed",
+    [theme.breakpoints.down("md")]: {
+      backgroundImage: `url("/assets/mobileBackground.jpg")`,
+      backgroundAttachment: "inherit",
+    },
+  },
+  estimateBtn: {
+    ...theme.typography.estimate,
+    backgroundColor: theme.palette.common.orange,
+    marginRight: "5rem",
+    marginLeft: "5rem",
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.dark,
+    },
+  },
+  btn: {
+    borderRadius: "40px",
+    height: "45px",
+    width: "145px",
+    margin: "5rem",
+    [theme.breakpoints.down("md")]: {
+      margin: "2rem 5rem",
+    },
+  },
+}));
+const CallToAction = ({ setValue }) => {
+  const classes = useStyle();
+  const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <Grid
+      container
+      justify={matchesSM ? "center" : "space-between"}
+      alignItems="center"
+      className={classes.background}
+      direction={matchesSM ? "column" : "row"}
+    >
+      <Grid item>
+        <Grid container direction="column">
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? "0" : "5rem",
+              textAlign: matchesSM && "center",
+            }}
+          >
+            <Typography variant="h2">
+              Simple Software.<br></br> Revolutionary Results.
+            </Typography>
+            <Typography variant="subtitle2">
+              Take advantage of the 21st Century
+            </Typography>
+            <Grid container justify={matchesSM ? "center" : null}>
+              <Button
+                variant="outlined"
+                className={[classes.btn, classes.learnBtnHero].join(" ")}
+                component={Link}
+                to="/revolution"
+                onClick={() => {
+                  setValue(2);
+                }}
+              >
+                <span style={{ marginRight: "10px" }}> Learn More</span>
+                <ButtonArrow
+                  width={15}
+                  height={15}
+                  fill={`${theme.palette.common.blue}`}
+                ></ButtonArrow>
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          className={[classes.estimateBtn, classes.btn].join(" ")}
+          component={Link}
+          to="/estimate"
+          onClick={() => {
+            setValue(5);
+          }}
+        >
+          Free Estimate
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default CallToAction;
---
+++ nextjs/src/ui/Theme.js
@@ -0,0 +1,113 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const arcBlue = "#0b79b9";
+const arcOrange = "#FFBA60";
+const arcGrey = "#868686";
+const arcGreyLight = "#ffffff";
+export default createMuiTheme({
+  palette: {
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`,
+    },
+    secondary: {
+      main: `${arcOrange}`,
+    },
+  },
+  typography: {
+    tab: {
+      fontFamily: "Railway",
+      textTransform: "none",
+      fontWeight: 700,
+      fontSize: "1rem",
+    },
+    estimate: {
+      fontFamily: "Pacifico",
+      fontSize: "1rem",
+      textTransform: "none",
+      color: "white",
+    },
+    h2: {
+      fontFamily: "Railway",
+      fontWeight: 700,
+      fontSize: "2.5rem",
+      color: `${arcBlue}`,
+      lineHeight: 1.5,
+    },
+    h3: {
+      fontFamily: "Pacifico",
+      fontWeight: 700,
+      fontSize: "2.5rem",
+      color: `${arcBlue}`,
+      lineHeight: 1.5,
+    },
+    h4: {
+      fontFamily: "Railway",
+      fontWeight: 700,
+      fontSize: "1.7rem",
+      color: `${arcBlue}`,
+      lineHeight: 1.5,
+    },
+    suntitle1: {
+      fontSize: "1.25rem",
+      fontWeight: 300,
+      color: arcGrey,
+    },
+    subtitle2: {
+      fontSize: "1.25rem",
+      fontWeight: 300,
+      color: "#ffffff",
+    },
+    learnBtn: {
+      border: `2px solid ${arcBlue}`,
+      color: arcBlue,
+      fontFamily: "Roboto",
+      fontWeight: "bold",
+      fontSize: "0.9rem",
+      textTransform: "none",
+    },
+    body1: {
+      color: arcGreyLight,
+
+      "&:selected, &:active span": {
+        opacity: 1,
+      },
+    },
+    body2: {
+      fontSize: "1.25rem",
+      color: arcGrey,
+      fontWeight: 300,
+    },
+  },
+  //Styling textInput If using the overrides key of the theme,
+  //you need to use the following style sheet name: MuiInputLabel
+  //https://material-ui.com/api/input-label/
+  overrides: {
+    MuiInputLabel: {
+      root: {
+        color: arcBlue,
+        fontSize: "1rem",
+      },
+    },
+    MuiInput: {
+      root: {
+        color: arcGrey,
+        fontWeight: 300,
+      },
+      underline: {
+        "&:before": {
+          borderBottom: `2px solid ${arcBlue}`,
+        },
+        "&:hover:not($disabled):not($error):before": {
+          borderBottom: `4px solid ${arcBlue}`,
+        },
+        "&:focused:before": {
+          borderBottom: `4px solid ${arcBlue}`,
+        },
+      },
+    },
+  },
+});
